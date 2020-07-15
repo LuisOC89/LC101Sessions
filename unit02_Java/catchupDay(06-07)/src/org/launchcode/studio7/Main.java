d8df75d8b2c5fd1276ea375a0cf32662840b0bf9
Luis O <humbledore@gmail.com>
2020-07-14 21:40:02 -0400

    Added catchupSession (studios06-07)

---
+++ unit02_Java/catchupDay(06-07)/src/org/launchcode/studio7/Main.java
@@ -0,0 +1,18 @@
+package org.launchcode.studio7;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // TODO: Declare and initialize a CD and a DVD object.
+        DVD aDVD = new DVD(
+                "KC and the Sunshine Band",
+                "4gb",
+                "Music and Fun",
+                "DVD");
+        // TODO: Call each CD and DVD method to verify that they work as expected.
+
+        aDVD.spinDisc();
+
+    }
+}
